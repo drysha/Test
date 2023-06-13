0788590d4c4a68051f8333dbec490cfd7cacfb8e
drysha <xelk2002@mail.ru>
2023-06-13 22:16:31 +0300

    Удалил

--- main.py
+++ main.py
@@ -5,13 +5,4 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 
 
-plt.scatter(raw_data[0][:,0], raw_data[0][:,1], c=raw_data[1]);
-model = KMeans(n_clusters=4)
-model.fit(raw_data[0])
-model.labels_
-model.cluster_centers_
-f, (ax1, ax2) = plt.subplots(1, 2, sharey=True,figsize=(10,6))
-ax1.set_title('Наши предсказания')
-ax1.scatter(raw_data[0][:,0], raw_data[0][:,1],c=model.labels_)
-ax2.set_title('Реальные значения')
-ax2.scatter(raw_data[0][:,0], raw_data[0][:,1],c=raw_data[1]);
+
